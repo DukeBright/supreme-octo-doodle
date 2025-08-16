a303b0fcb25d69be4f3bf6cfd88fe25e917035b1
DukeBright <shebrokemy89@gmail.com>
2025-08-16 19:53:05 +0000

    Add event logging module

---
+++ event_logger.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(filename='events.log', level=logging.INFO,
+                    format='%(asctime)s %(message)s')
+
+def log_event(event):
+    logging.info(event)
